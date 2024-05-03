1f50b8025a15ae48a2513533485d58d19e7105d8
Dar <fauzi@infotech.co.id>
2024-05-03 17:35:38 +0700

    a

--- lib/features/pages/home_page.dart
+++ lib/features/pages/home_page.dart
@@ -12,7 +12,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: ,
+        appBar: AppBar(),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
